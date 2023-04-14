76c12bf1ae31fcbc2b9a3c76e9b2a6dfc06bf3b7
Lorenzo Radaele s301165 <s301165@studenti.polito.it>
2023-04-14 10:37:31 +0200

    Feat: Lab2 fixed routes

--- server/src/main/kotlin/it/polito/wa2/g27/server/ServerApplication.kt
+++ server/src/main/kotlin/it/polito/wa2/g27/server/ServerApplication.kt
@@ -2,10 +2,35 @@ package it.polito.wa2.g27.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.server.ErrorPage
+import org.springframework.boot.web.server.WebServerFactoryCustomizer
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class ServerApplication
 
 fun main(args: Array<String>) {
     runApplication<ServerApplication>(*args)
+}
+
+@Configuration
+public class WebApplicationConfig : WebMvcConfigurer {
+
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/notFound").setViewName("forward:/index.html");
+    }
+
+
+    @Bean
+    fun containerCustomizer(): WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+        return WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+                factory -> factory?.addErrorPages(ErrorPage(HttpStatus.NOT_FOUND, "/notFound"));
+        };
+    }
+
 }
