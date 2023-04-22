2ff02f705dae156c43e714854d44488da1ba16d8
AlexZampa <s301132@studenti.polito.it>
2023-04-22 11:39:54 +0200

    feat: created table Ticket and TicketHistory

---
+++ ticketing/src/main/kotlin/it/polito/wa2/g27/ticketing/ticket/Ticket.kt
@@ -0,0 +1,23 @@
+package it.polito.wa2.g27.ticketing.ticket
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "ticket")
+class Ticket {
+    @Id @GeneratedValue
+    var id : Int = 0
+    var productId : String = ""
+    var category: String = ""
+    var priority: Int = 0
+    var description: String = ""
+    var userId: String = ""
+    var expertId: String = ""
+    var chatId: String = ""
+    @OneToMany(mappedBy = "id")
+    var ticketHistory: Set<TicketHistory>? = null
+}
---
+++ ticketing/src/main/kotlin/it/polito/wa2/g27/ticketing/ticket/TicketHistory.kt
@@ -0,0 +1,19 @@
+package it.polito.wa2.g27.ticketing.ticket
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "ticketHistory")
+class TicketHistory {
+    @Id @GeneratedValue
+    var id: Int = 0
+    @ManyToOne
+    var ticket: Ticket? = null
+    var status: String = ""
+    var date: LocalDateTime = LocalDateTime.now()
+}
