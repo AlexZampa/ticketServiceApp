479162ccba365fd1bebec4326172d4ed8f00c624
Alessandro Zamparutti s301132 <s301132@studenti.polito.it>
2023-04-24 10:49:22 +0200

    refactor: deleted ticketing module

--- ticketing/src/main/kotlin/it/polito/wa2/g27/ticketing/ticket/TicketHistoryService.kt
+++
@@ -1,6 +0,0 @@
-package it.polito.wa2.g27.ticketing.ticket
-
-interface TicketHistoryService {
-    fun insertNewTicketStatus()
-    fun getAllTicketHistory(): List<TicketHistory>
-}
--- ticketing/src/main/kotlin/it/polito/wa2/g27/ticketing/ticket/TicketRepository.kt
+++
@@ -1,7 +0,0 @@
-package it.polito.wa2.g27.ticketing.ticket
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface TicketRepository: JpaRepository<Ticket, Int> {}
