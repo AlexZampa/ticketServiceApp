d12fad849eb078386360bf58597f314519538c3e
FabioTrovero <s301587@studenti.polito.it>
2023-07-25 19:34:10 +0200

    feat: remove kafka and web socket

--- server/src/main/kotlin/it/polito/wa2/g27/server/messages/MessageController.kt
+++ server/src/main/kotlin/it/polito/wa2/g27/server/messages/MessageController.kt
@@ -1,6 +1,5 @@
 package it.polito.wa2.g27.server.messages
 
-import WSConfig
 import io.micrometer.observation.annotation.Observed
 import it.polito.wa2.g27.server.messages.attachments.AttachmentDTO
 import it.polito.wa2.g27.server.messages.attachments.AttachmentService
@@ -12,35 +11,8 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import java.io.*
 import java.time.LocalDateTime
-import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.Producer
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.serialization.StringSerializer
-import java.util.Properties;
-import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.apache.kafka.clients.consumer.Consumer
-import org.apache.kafka.common.serialization.StringDeserializer
-import java.time.Duration
 import org.springframework.web.socket.*
 import org.springframework.web.socket.config.annotation.*
-
-private fun createConsumer(): Consumer<String, String> {
-    val props = Properties()
-    props["bootstrap.servers"] = "host.docker.internal:9092"
-    props["key.deserializer"] = StringDeserializer::class.java
-    props["value.deserializer"] = StringDeserializer::class.java
-    return KafkaConsumer(props)
-}
-private fun createProducer(): Producer<String, String> {
-    val props = Properties()
-    props["bootstrap.servers"] = "host.docker.internal:9092"
-    props["group.id"] = "hello-world"
-    props["key.serializer"] = StringSerializer::class.java
-    props["value.serializer"] = StringSerializer::class.java
-    return KafkaProducer<String, String>(props)
-}
-val producer = createProducer()
-val wsconfig = WSConfig()
 
 @RestController
 @Observed
@@ -73,28 +45,7 @@ class MessageController(private val messageService: MessageService, private val 
 
         val messageDTO = MessageDTO(0, id, senderId, receiverId, text, LocalDateTime.now(), attachments.map {
             AttachmentDTO(0, it.originalFilename ?:"", it.contentType?:"", it.size, it.bytes, 0) })
-        val future = producer.send(ProducerRecord(ticketId.toString(), id.toString(), messageDTO.toString()))
-        future.get()
         messageService.sendMessage(messageDTO)
-    }
-
-    @CrossOrigin(origins = ["http://localhost:3000"])
-    @PostMapping("/authenticated/tickets/{id}/subscribe")
-    @ResponseStatus(HttpStatus.CREATED)
-    fun subscribe(@PathVariable id: Int) {
-        val consumer = createConsumer()
-        consumer.subscribe(listOf(id.toString()))
-
-        while (true) {
-            val records = consumer.poll(Duration.ofSeconds(1))
-            println("Consumed ${records.count()} records")
-
-            records.iterator().forEach {
-                val message = it.value()
-                println("Message: $message")
-
-            }
-        }
     }
 }
 
